be22575cdc16cb7331e2f0b5715f77c20d3916e5
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-10 17:35:28 +0200

    update d03

--- rendu03/ex09/ft_sort_integer_table.c
+++ rendu03/ex09/ft_sort_integer_table.c
@@ -6,15 +6,15 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/10 15:02:52 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/10 17:31:57 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/10 17:35:14 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_sort_integer_table(int *tab, int size)
+void	ft_sort_integer_table(int *tab, int size)
 {
-	int i;
-	int j;
-	int temp;
+	int		i;
+	int		j;
+	int		temp;
 
 	i = 0;
 	while (i < (size - 1))
@@ -22,11 +22,11 @@ void ft_sort_integer_table(int *tab, int size)
 		j = 0;
 		while (j < (size - 1))
 		{
-			if (tab[j] > tab[j+1])
+			if (tab[j] > tab[j + 1])
 			{
 				temp = tab[j];
-				tab[j] = tab[j+1];
-				tab[j+1] = temp;
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
 			}
 			j++;
 		}
