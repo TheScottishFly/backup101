53c6c3eb2326063365d85e46df59eb5ebd5ceef1
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-10 17:32:09 +0200

    update d02 ex06

--- rendu03/ex09/ft_sort_integer_table.c
+++ rendu03/ex09/ft_sort_integer_table.c
@@ -6,42 +6,30 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/10 15:02:52 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/10 17:01:15 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/10 17:31:57 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void ft_sort_integer_table(int *tab, int size)
 {
 	int i;
+	int j;
 	int temp;
-	int in_order;
 
 	i = 0;
-	in_order = 0;
 	while (i < (size - 1))
 	{
-		while (in_order == 0)
+		j = 0;
+		while (j < (size - 1))
 		{
-			if (tab[i] < tab[i+1])
+			if (tab[j] > tab[j+1])
 			{
-				temp = tab[i];
-				tab[i] = tab[i+1];
-				tab[i] = temp;
-				in_order = 1;
+				temp = tab[j];
+				tab[j] = tab[j+1];
+				tab[j+1] = temp;
 			}
+			j++;
 		}
-		size--;
+		i++;
 	}
-}
-
-int main(void)
-{
-	int tab[] = {1, 8, 5, 3, 7, 9, 6};
-
-	ft_sort_integer_table(tab, 7);
-	for(int i = 0; i < 7; i++)
-		printf("%d", tab[i]);
-	return (0);
 }
