d7593b3852c8c2b7b9b435b07d436e8e1d77f7ad
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-11 12:59:28 +0200

    update d03 and d05

--- rendu03/ex08/ft_atoi.c
+++ rendu03/ex08/ft_atoi.c
@@ -6,28 +6,40 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/08 16:25:14 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/10 19:37:13 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/11 12:57:39 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(char *str)
+int		ft_strlen(char *str)
 {
-	int		i;
-	int		len;
-	int		result;
+	int i;
 
 	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int		ft_atoi(char *str)
+{
+	inti;
+	int     j;
+	intlen;
+	intresult;
+
+	j = 0;
+	len = ft_strlen(str);
 	result = 0;
-	len = 0;
-	while (str[len] != '\0')
-		len++;
-	while (i < len)
+	while (str[j] != '-' && (str[j] <= 48 || str[j] >= 57))
+		j++;
+	i = j;
+	while (i <= len)
 	{
-		if (str[i] != '-')
+		if (str[i] != '-' && str[i] >= 48 && str[i] <= 57)
 			result = result * 10 + (str[i] - '0');
 		i++;
 	}
-	if (str[0] == '-')
+	if (str[j] == '-')
 		result *= -1;
 	return (result);
 }
