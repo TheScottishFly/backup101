9835e81535b2bc8ae8c91d09a5ee0cf8e969c32e
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-12 20:21:39 +0200

    update d07

--- rendu07/ex02/ft_ultimate_range.c
+++ rendu07/ex02/ft_ultimate_range.c
@@ -1,36 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_range.c                                         :+:      :+:    :+:   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/12 19:57:21 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/12 20:16:34 by grosnet-         ###   ########.fr       */
+/*   Created: 2017/09/12 20:15:02 by grosnet-          #+#    #+#             */
+/*   Updated: 2017/09/12 20:20:06 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int		*ft_range(int min, int max)
+int		ft_ultimate_range(int **range, int min, int max)
 {
-	int		*int_tab;
 	int		i;
 
 	i = 0;
 	if (min >= max)
 	{
-		int_tab = NULL;
-		return (int_tab);
+		range = NULL;
+		return (range);
 	}
-	int_tab = malloc((max - min) * sizeof(int));
-	if (int_tab == NULL)
-		exit(0);
+	range = malloc((max - min) * sizeof(int));
+	if (range == NULL)
+		return (0);
 	while (min < max)
 	{
-		int_tab[i] = min;
+		range[i] = min;
 		min++;
 		i++;
 	}
-	return (int_tab);
+	return (i);
 }
