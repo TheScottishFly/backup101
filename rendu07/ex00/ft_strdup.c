354f61cfd28d4062c3373bb32d859da0e94fbf7a
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-12 20:11:22 +0200

    update d07

--- rendu07/ex00/ft_strdup.c
+++ rendu07/ex00/ft_strdup.c
@@ -6,39 +6,29 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 19:39:11 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/12 19:53:13 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/12 20:07:45 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
 
 char	*ft_strdup(char *src)
 {
-	int length;
-	int i;
-	char *new_str;
+	int		length;
+	int		i;
+	char	*new_str;
 
 	length = 0;
 	i = 0;
-	while(src[length])
+	while (src[length])
 		length++;
 	new_str = malloc((length - 1) * sizeof(char));
 	if (new_str == NULL)
 		exit(0);
-	while(src[i])
+	while (src[i])
 	{
 		new_str[i] = src[i];
-	i++;
+		i++;
 	}
 	return (new_str);
 }
-
-int main()
-{
-	char str[20] = "Hello World";
-	printf("%s\n", ft_strdup(str));
-
-	return 0;
-}
-
--- rendu07/ex01/ft_range.c
+++ rendu07/ex01/ft_range.c
@@ -6,11 +6,24 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 19:57:21 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/12 19:57:45 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/12 20:07:56 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 int		*ft_range(int min, int max)
 {
-	
+	int		*int_tab;
+	int		i;
+
+	i = 0;
+	int_tab = malloc((max - min) * sizeof(int));
+	while (min < max)
+	{
+		int_tab[i] = min;
+		min++;
+		i++;
+	}
+	return (int_tab);
 }
