6fc69c480c861daa43189b235633a296526146b7
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-15 07:04:27 +0200

    update d07

--- rendu07/ex04/ft_split_whitespaces.c
+++ rendu07/ex04/ft_split_whitespaces.c
@@ -6,12 +6,11 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/13 08:50:27 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/14 13:03:12 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/15 07:04:16 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 int		is_separator(char c)
 {
@@ -22,7 +21,7 @@ int		is_separator(char c)
 
 int		position_next_word(char *str, int position)
 {
-	while (is_separator(str[position]) == 1)
+	while (is_separator(str[position]))
 		position++;
 	return (position);
 }
@@ -34,9 +33,9 @@ int		get_length_next_word(char *str)
 
 	i = 0;
 	j = 0;
-	while (is_separator(str[j]) == 1)
+	while (is_separator(str[j]))
 		j++;
-	while (is_separator(str[j + i]) == 0)
+	while (!(is_separator(str[j + i])))
 		i++;
 	return (i + 1);
 }
@@ -50,10 +49,10 @@ int		get_count_words(char *str)
 	j = 0;
 	while (str[i])
 	{
-		if (i == 0 && is_separator(str[i]) == 0)
+		if (i == 0 && !(is_separator(str[i])))
 			j++;
-		else if (i != 0 && is_separator(str[i]) == 0 &&
-				is_separator(str[i - 1]) == 1)
+		else if (i != 0 && !(is_separator(str[i])) &&
+				is_separator(str[i - 1]))
 			j++;
 		i++;
 	}
@@ -65,35 +64,26 @@ char	**ft_split_whitespaces(char *str)
 	char	**pt_tab;
 	int		i;
 	int		j;
-	int k;
+	int		k;
 
 	i = 0;
 	j = 0;
 	k = 0;
 	pt_tab = malloc((get_count_words(str) + 1) * sizeof(char*));
-	while (i <= get_count_words(str))
+	while (i < get_count_words(str))
 	{
 		pt_tab[i] = malloc((get_length_next_word(str) + 1) * sizeof(char));
 		k = position_next_word(str, k);
-		while (is_separator(str[k]) == 0)
+		while (!(is_separator(str[k])))
 		{
-			pt_tab[i][j] = str[k];
+			pt_tab[i][j] = str[k++];
 			j++;
-			k++;
 		}
 		pt_tab[i][j] = '\0';
 		i++;
 		j = 0;
 	}
-	pt_tab[i] = malloc(1 * sizeof(int));
+	pt_tab[i] = malloc(1 * sizeof(char));
 	pt_tab[i][j] = 0;
 	return (pt_tab);
-}
-
-int		main()
-{
-	char **test = ft_split_whitespaces("    test    tes    test tes te");
-	for (int i = 0; i < 6; i++)
-		printf("%s\n", test[i]);
-	return (0);
 }
