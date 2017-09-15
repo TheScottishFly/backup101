6d3cd247f3810c186c67a1efc16d50ef780daf8e
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-15 12:08:36 +0200

    update d08

---
+++ rendu08/ex00/ft_split_whitespaces.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_whitespaces.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/13 08:50:27 by grosnet-          #+#    #+#             */
+/*   Updated: 2017/09/15 08:40:34 by grosnet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		is_separator(char c)
+{
+	if (c != ' ' && c != '\n' && c != '\t')
+		return (0);
+	return (1);
+}
+
+int		position_next_word(char *str, int position)
+{
+	while (is_separator(str[position]))
+		position++;
+	return (position);
+}
+
+int		get_length_next_word(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (is_separator(str[j]))
+		j++;
+	while (!(is_separator(str[j + i])))
+		i++;
+	return (i + 1);
+}
+
+int		get_count_words(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (i == 0 && !(is_separator(str[i])))
+			j++;
+		else if (i != 0 && !(is_separator(str[i])) &&
+				is_separator(str[i - 1]))
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+char	**ft_split_whitespaces(char *str)
+{
+	char	**pt_tab;
+	int		i;
+	int		j;
+	int		k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	pt_tab = malloc((get_count_words(str) + 1) * sizeof(char*));
+	while (i < get_count_words(str))
+	{
+		pt_tab[i] = malloc((get_length_next_word(str) + 1) * sizeof(char));
+		k = position_next_word(str, k);
+		while (!(is_separator(str[k])))
+			pt_tab[i][j++] = str[k++];
+		pt_tab[i++][j] = '\0';
+		j = 0;
+	}
+	pt_tab[i] = malloc(1 * sizeof(char));
+	pt_tab[i][j] = 0;
+	return (pt_tab);
+}
---
+++ rendu08/ex02/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/15 11:19:33 by grosnet-          #+#    #+#             */
+/*   Updated: 2017/09/15 12:07:55 by grosnet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+# define TRUE 1
+# define FALSE 0
+# define EVEN(nb) nb % 2 == 0
+
+typedef int		t_bool;
+
+#endif
