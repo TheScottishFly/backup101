328a121dc1b9d3c83ab8b87379a128a23467118b
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-16 07:30:41 +0200

    update d09

--- day09/ex01/ft_takes_place.c
+++ day09/ex01/ft_takes_place.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_takes_place.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/16 07:30:02 by grosnet-          #+#    #+#             */
+/*   Updated: 2017/09/16 07:30:05 by grosnet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 void		ft_takes_place(int hour)
@@ -8,22 +20,22 @@ void		ft_takes_place(int hour)
 
 	if (hour >= 12)
 	{
-		x = "P.M.";
-		hour - hour - 12;
+		x_letter = "P.M.";
+		hour = hour - 12;
 		if (hour == 0)
 			hour = 12;
 	}
 	else
-		x = "A.M."
+		x_letter = "A.M.";
 	if (hour + 1 >= 12)
-		y = "P.M."
+		y_letter = "P.M.";
 	else
-		y = "A.M."
+		y_letter = "A.M.";
 	if (hour + 1 > 12)
 		y = 1;
 	else
-		hour + 1;
-	if (x[0] == 'P' && hour == 11)
+		y = hour + 1;
+	if (x_letter[0] == 'P' && hour == 11)
 		y_letter = "A.M.";
 	printf("THE FOLLOWING TAKES PLACE BETWEEN ");
 	printf("%d.00 %s AND %d.00 %s\n", hour, x_letter, y, y_letter);
