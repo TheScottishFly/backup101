e73b2500e7c91dbfd21e8d300e78baed2588a717
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-16 15:54:07 +0200

    update d09

--- day09/ex05/ft_antidote.c
+++ day09/ex05/ft_antidote.c
@@ -6,16 +6,16 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/16 08:20:32 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/16 08:36:00 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/16 15:54:00 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_antidote(int i, int j, int k)
 {
-	if ((i < j && i > k) || (i > j && i < k))
+	if ((i <= j && i >= k) || (i >= j && i <= k))
 		return (i);
-	else if ((j < i && j > k) || (j > i && j < k))
+	else if ((j <= i && j >= k) || (j >= i && j <= k))
 		return (j);
-	else if ((k < i && k > j) || (k > i && k < j))
+	else if ((k <= i && k >= j) || (k >= i && k <= j))
 		return (k);
 }
