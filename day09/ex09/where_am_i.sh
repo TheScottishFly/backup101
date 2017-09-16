ef27a08252a699c1af4715d275ac4cd163eda571
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-16 10:00:53 +0200

    update d09

--- day09/ex09/where_am_i.sh
+++ day09/ex09/where_am_i.sh
@@ -1,4 +1,6 @@
-if [ ifconfig | grep "inet " | sed "s/.*inet //g" | cut -d' ' -f1 | wc -l  > 0 ]
+$nb_lines = ifconfig | grep "inet " | sed "s/.*inet //g" | cut -d' ' -f1 | wc -l
+
+if [ $nb_lines > 0 ]
 then
 	ifconfig | grep "inet " | sed "s/.*inet //g" | cut -d' ' -f1
 else
