64c1c2953118e87b56da45e7dae5d43b9e550650
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-16 10:17:02 +0200

    update d09

--- day09/ex07/ft_collatz_conjecture.c
+++ day09/ex07/ft_collatz_conjecture.c
@@ -6,23 +6,17 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/16 08:45:48 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/16 09:54:27 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/16 10:16:19 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_collatz_conjecture(unsigned int base)
 {
-	int i;
-
-	i = 0;
-	while (base != 1)
-	{
-		if (base % 2 == 0)
-			base /= 2;
-		else
-			base = base * 3 + 1;
-		i++;
-	}
-	return (i);
-
+	if (base <= 1)
+		return (0);
+	if (base % 2 == 0)
+		base /= 2;
+	else
+		base = base * 3 + 1;
+	return (ft_collatz_conjecture(base) + 1);
 }
