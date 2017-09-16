bad67ece1933c0e6472946da381aae588761c106
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-16 12:10:21 +0200

    update d09

---
+++ day09/ex12/ft_door.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_door.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/16 11:37:25 by grosnet-          #+#    #+#             */
+/*   Updated: 2017/09/16 12:09:34 by grosnet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_door.h"
+
+void	ft_putstr(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i])
+	{
+		write(1, str, 1);
+		i++;
+	}
+}
+
+int		close_door(t_door *door)
+{
+	ft_putstr("Door closing...");
+	door->state = CLOSE;
+	return (TRUE);
+}
+
+int		open_door(t_door *door)
+{
+	ft_putstr("Door opening...");
+	door->state = OPEN;
+	return (TRUE);
+}
+
+int		is_door_close(t_door *door)
+{
+	ft_putstr("Door is close ?");
+	if (door->state == CLOSE)
+		return (TRUE);
+	else
+		return (FALSE);
+}
+
+int		is_door_open(t_door *door)
+{
+	ft_putstr("Door is open ?");
+	if (door->state == OPEN)
+		return (TRUE);
+	else
+		return (FALSE);
+}
--- day09/ex12/ft_door.h
+++ day09/ex12/ft_door.h
@@ -6,8 +6,21 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/16 11:06:56 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/16 11:06:58 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/16 12:10:10 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_DOOR_H
+# define FT_DOOR_H
+# define OPEN 0
+# define CLOSE 1
+# define TRUE 1
+# define FALSE 0
+# include <unistd.h>
 
+typedef struct	s_door
+{
+	int state;
+}				t_door;
+
+#endif
