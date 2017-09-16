fcd76ae0b6005f19b152fa26a06c21f87ccd8434
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-16 09:55:41 +0200

    update d09

--- day09/ex07/ft_collatz_conjecture.c
+++ day09/ex07/ft_collatz_conjecture.c
@@ -6,14 +6,23 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/16 08:45:48 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/16 08:47:27 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/16 09:54:27 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_collatz_conjecture(unsigned int base)
 {
-	if (base % 2 == 0)
-		return (base / 2);
-	else
-		return ((base * 3) + 1);
+	int i;
+
+	i = 0;
+	while (base != 1)
+	{
+		if (base % 2 == 0)
+			base /= 2;
+		else
+			base = base * 3 + 1;
+		i++;
+	}
+	return (i);
+
 }
--- day09/ex09/where_am_i.sh
+++ day09/ex09/where_am_i.sh
@@ -1,1 +1,6 @@
-ifconfig | grep "inet " | sed "s/.*inet //g" | cut -d' ' -f1
+if [ ifconfig | grep "inet " | sed "s/.*inet //g" | cut -d' ' -f1 | wc -l  > 0 ]
+then
+	ifconfig | grep "inet " | sed "s/.*inet //g" | cut -d' ' -f1
+else
+	echo "Je suis perdu !"
+fi
