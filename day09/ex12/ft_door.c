b6026c827dc06a71bea1100ba9f81871fe8ff37e
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-16 12:22:34 +0200

    update d09

--- day09/ex12/ft_door.c
+++ day09/ex12/ft_door.c
@@ -6,7 +6,7 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/16 11:37:25 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/16 12:09:34 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/16 12:22:25 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,32 @@
 void	ft_putstr(char *str)
 {
 	int i;
-	
+
 	i = 0;
 	while (str[i])
 	{
-		write(1, str, 1);
+		write(1, &str[i], 1);
 		i++;
 	}
 }
 
 int		close_door(t_door *door)
 {
-	ft_putstr("Door closing...");
+	ft_putstr("Door closing...\n");
 	door->state = CLOSE;
 	return (TRUE);
 }
 
 int		open_door(t_door *door)
 {
-	ft_putstr("Door opening...");
+	ft_putstr("Door opening...\n");
 	door->state = OPEN;
 	return (TRUE);
 }
 
 int		is_door_close(t_door *door)
 {
-	ft_putstr("Door is close ?");
+	ft_putstr("Door is close ?\n");
 	if (door->state == CLOSE)
 		return (TRUE);
 	else
@@ -49,7 +49,7 @@ int		is_door_close(t_door *door)
 
 int		is_door_open(t_door *door)
 {
-	ft_putstr("Door is open ?");
+	ft_putstr("Door is open ?\n");
 	if (door->state == OPEN)
 		return (TRUE);
 	else
--- day09/ex12/ft_door.h
+++ day09/ex12/ft_door.h
@@ -6,7 +6,7 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/16 11:06:56 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/16 12:10:10 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/16 12:21:52 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@
 # define CLOSE 1
 # define TRUE 1
 # define FALSE 0
+# define EXIT_SUCCESS 0
 # include <unistd.h>
 
 typedef struct	s_door
 {
 	int state;
 }				t_door;
+
+void			ft_putstr(char *str);
+int				close_door(t_door *door);
+int				open_door(t_door *door);
+int				is_door_close(t_door *door);
+int				is_door_open(t_door *door);
 
 #endif
