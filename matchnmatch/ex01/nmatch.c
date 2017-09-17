b80127cd6f55dacb1d83962df3d91c1f7424e7c6
TheScottishFly <none@gmail.com>
2017-09-17 22:51:28 +0200

    be careful

--- matchnmatch/ex01/nmatch.c
+++ matchnmatch/ex01/nmatch.c
@@ -20,13 +20,13 @@ int		nmatch(char *s1, char *s2)
 	if (*s1 == '\0' && *s1 == *s2)
 		return (1);
 	else if (*s1 == *s2 && *s2 != '*')
-		return (1 * match(s1 + 1, s2 + 1));
+		return (1 * nmatch(s1 + 1, s2 + 1));
 	else if (*s1 == *s2 && *s2 == '*')
-		return (1 * match(s1 + 1, s2));
+		return (1 * nmatch(s1 + 1, s2));
 	else if (*s1 != '\0' && *s2 == '*')
-		return (1 * ((match(s1 + 1, s2) + (match(s1, s2 + 1)))));
+		return (1 * ((nmatch(s1 + 1, s2) + (nmatch(s1, s2 + 1)))));
 	else if (*s1 == '\0' && *s2 == '*')
-		return (1 * match(s1, s2 + 1));
+		return (1 * nmatch(s1, s2 + 1));
 	else
 		return (0);
 }
