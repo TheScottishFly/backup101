a9080a75836d7021225037aac8fc42f43d3d5e65
TheScottishFly <none@gmail.com>
2017-09-17 22:39:08 +0200

    putain de matchnmatch

--- matchnmatch/ex00/match.c
+++ matchnmatch/ex00/match.c
@@ -17,16 +17,16 @@ int		match(char *s1, char *s2)
 		while (*(s2 + 1) == '*')
 			s2++;
 	}
-	if (*(s1 + 1) == '\0' && *s2 == '*' && *s1 != *(s2 + 1))
-		return (0);
-	else if (*s1 == '\0' && *s1 == *s2)
+	if (*s1 == '\0' && *s1 == *s2)
 		return (1);
-	else if (*s1 != *s2 && *s2 == '*' && *(s2 + 1) == *s1)
-		return (1 * match(s1, s2 + 1));
-	else if (*s1 == *s2)
+	else if (*s1 == *s2 && *s2 != '*')
 		return (1 * match(s1 + 1, s2 + 1));
-	else if (*s1 != *s2 && *s2 == '*')
+	else if (*s1 == *s2 && *s2 == '*')
 		return (1 * match(s1 + 1, s2));
+	else if (*s1 != '\0' && *s2 == '*')
+		return (1 * ((match(s1 + 1, s2) || (match(s1, s2 + 1)))));
+	else if (*s1 == '\0' && *s2 == '*')
+		return (1 * match(s1, s2 + 1));
 	else
 		return (0);
 }
