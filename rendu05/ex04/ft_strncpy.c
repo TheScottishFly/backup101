e1bfd8d32916bd8a458da747f367c7d91b2a302f
TheScottishFly <none@gmail.com>
2017-09-17 23:03:46 +0200

    update d5

--- rendu05/ex04/ft_strncpy.c
+++ rendu05/ex04/ft_strncpy.c
@@ -20,7 +20,10 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 		i++;
 	}
-	while (dest[i] != '\0')
-		dest[i++] = '\0';
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
