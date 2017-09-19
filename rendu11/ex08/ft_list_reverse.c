714c49f39304af6ac49e2af29ab507dd3e5eb5c7
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-19 18:34:28 +0200

    update d11

--- rendu11/ex08/ft_list_reverse.c
+++ rendu11/ex08/ft_list_reverse.c
@@ -6,13 +6,13 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 15:15:27 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/19 15:17:48 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/19 17:25:04 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void	ft_list_clear(t_list **begin_list)
+void	ft_list_reverse(t_list **begin_list)
 {
 	t_list *buf;
 	t_list *next;
@@ -21,7 +21,7 @@ void	ft_list_clear(t_list **begin_list)
 	if (buf->next)
 	{
 		next = buf->next;
-		ft_list_clear(&next);
+		ft_list_reverse(&next);
 	}
 	buf->next = *begin_list;
 }
