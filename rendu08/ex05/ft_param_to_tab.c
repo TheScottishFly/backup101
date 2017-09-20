e36aee6fc0be9b1952fb4cc62435b83ff7c485f0
TheScottishFly <none@gmail.com>
2017-09-20 21:50:52 +0200

    update d8

--- rendu08/ex05/ft_param_to_tab.c
+++ rendu08/ex05/ft_param_to_tab.c
@@ -13,21 +13,6 @@
 #include <stdlib.h>
 #include "ft_stock_par.h"
 
-char				*ft_strcpy(char *dest, char *src, int size)
-{
-	int i;
-
-	i = 0;
-	dest = malloc(size + 1 * sizeof(char));
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 int					ft_strlen(char *str)
 {
 	int len;
@@ -38,21 +23,39 @@ int					ft_strlen(char *str)
 	return (len);
 }
 
+char	*ft_strdup(char *str)
+{
+	char	*new_str;
+	int		length;
+	int		i;
+
+	i = 0;
+	length = ft_strlen(str);
+	new_str = (char*)malloc(sizeof(char) * (length + 1));
+	while (i < length)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	new_str[length] = '\0';
+	return (new_str);
+}
+
 struct s_stock_par	*ft_param_to_tab(int ac, char **av)
 {
 	t_stock_par	*tab;
 	int			i;
 
-	tab = malloc(ac + 1 * sizeof(t_stock_par));
+	tab = (t_stock_par*)malloc(sizeof(tab) * (ac + 1));
 	i = 0;
-	while (i < ac)
+	while (i < size)
 	{
 		tab[i].size_param = ft_strlen(av[i]);
 		tab[i].str = av[i];
-		tab[i].copy = ft_strcpy(tab[i].copy, av[i], tab[i].size_param);
-		tab[i].tab = ft_split_whitespaces(tab[i].copy);
+		tab[i].copy = ft_strdup(av[i]);
+		tab[i].tab = ft_split_whitespaces(av[i]);
 		i++;
 	}
-	tab[i].str = NULL;
+	tab[i].str = "\0";
 	return (tab);
 }
