ef7bac92228ed6269ff820afd7e9d48cac2047cd
Gilian Rosnet boutinet <grosnet-@z1r10p5.le-101.fr>
2017-09-21 13:29:40 +0200

    update d10

--- rendu10/ex06/fdoop.c
+++ rendu10/ex06/fdoop.c
@@ -6,21 +6,55 @@
 /*   By: grosnet- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 13:02:59 by grosnet-          #+#    #+#             */
-/*   Updated: 2017/09/21 13:04:39 by grosnet-         ###   ########.fr       */
+/*   Updated: 2017/09/21 13:29:20 by grosnet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	is_operator(char c)
+int		is_operator(char c)
 {
-	
+	if (c != '+' && c != '-' && c != '*' && c != '/')
+		return (0);
+	return (1);
 }
 
 int		ft_atoi(char *str)
 {
+	int nb;
+	int neg;
 
+	nb = 0;
+	while (*str <= 32)
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			neg = 1;
+		else
+			neg = 0;
+		str++;
+	}
+	while (*str >= 48 && *str <= 57)
+	{
+		nb = nb * 10 + *str - '0';
+		str++;
+	}
+	if (neg == 1)
+		nb = -nb;
+	return (nb);
 }
 
 char	*ft_itoa(int nb)
 {
+	char str[11];
+	int i;
+	int div;
 
+	i = 0;
+	while (nb > 10)
+	{
+		div = nb / 10;
+		str[i] = nb % div + '0';
+		nb = div;
+		i++;
+	}
 }
